1b15e07d28d5a34bf448164cc55c024025792fd3
mishuk09 <mishukinfo09@gmail.com>
2024-11-10 23:15:24 +0530

    update front section

--- Front-End/src/components/Checkout.js
+++ Front-End/src/components/Checkout.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { useCart } from './CartContext';
 import axios from 'axios';
+import am from './Footer/img/am.svg';
+import master from './Footer/img/master.svg';
+import visa from './Footer/img/visa.svg';
 
 const Checkout = () => {
     const { cartItems, setCartItems } = useCart();
@@ -14,7 +17,7 @@ const Checkout = () => {
         landmark: '',
     });
     const [errors, setErrors] = useState({});
-    const [paymentMethod, setPaymentMethod] = useState('Cash on delivery'); // State to handle payment method
+    const [paymentMethod, setPaymentMethod] = useState('Card Payment');
     const [cardDetails, setCardDetails] = useState({ cardNumber: '', expiryDate: '', cvv: '' });
 
     const handleChange = (e) => {
@@ -23,10 +26,6 @@ const Checkout = () => {
             ...formData,
             [name]: value,
         });
-    };
-
-    const handlePaymentMethodChange = (e) => {
-        setPaymentMethod(e.target.value);
     };
 
     const handleCardChange = (e) => {
@@ -48,11 +47,9 @@ const Checkout = () => {
 
     const validateCardDetails = () => {
         const newErrors = {};
-        if (paymentMethod === 'Card Payment') {
-            if (!cardDetails.cardNumber) newErrors.cardNumber = 'Card Number is required';
-            if (!cardDetails.expiryDate) newErrors.expiryDate = 'Expiry Date is required';
-            if (!cardDetails.cvv) newErrors.cvv = 'CVV is required';
-        }
+        if (!cardDetails.cardNumber) newErrors.cardNumber = 'Card Number is required';
+        if (!cardDetails.expiryDate) newErrors.expiryDate = 'Expiry Date is required';
+        if (!cardDetails.cvv) newErrors.cvv = 'CVV is required';
         return newErrors;
     };
 
@@ -68,15 +65,14 @@ const Checkout = () => {
         const orderData = {
             ...formData,
             cartItems,
-            totalAmount: calculateTotal() + 100, // Assuming delivery charge is 100
+            totalAmount: calculateTotal() + 100,
             paymentMethod,
-            cardDetails, // Send card details only if Card Payment is selected
+            cardDetails, // Send card details for card payment
         };
 
         try {
             const response = await axios.post('http://localhost:5000/item/orders', orderData);
             console.log(response.data);
-            // Clear form fields after successful order placement
             setFormData({
                 fullName: '',
                 email: '',
@@ -87,11 +83,9 @@ const Checkout = () => {
                 landmark: '',
             });
             setCardDetails({ cardNumber: '', expiryDate: '', cvv: '' });
-            // Clear cart items after successful order placement
             setCartItems([]);
         } catch (error) {
             console.error('Error placing order:', error);
-            // Handle error (e.g., show an error message)
         }
     };
 
@@ -100,36 +94,36 @@ const Checkout = () => {
     };
 
     return (
-        <div className=" mx-auto p-4 lg:px-0">
-            <div className="text-center pb-6  border-b-2  ">
+        <div className="mx-auto p-4 lg:px-0">
+            <div className="text-center pb-6 border-b-2">
                 <h1 className="text-2xl mt-10 font-bold">Checkout</h1>
             </div>
-            <div className='flex container flex-col lg:flex-row gap-10 '>
-                <div className='w-full border-r-2 pe-10 pt-6'>
+            <div className="flex container flex-col lg:flex-row gap-10">
+                <div className="w-full border-r-2 pe-10 pt-6">
                     <form onSubmit={handleSubmit}>
                         <div className="mb-6">
                             <h2 className="text-xl font-semibold mb-4">1. General Information</h2>
-                            <label className="block mb-2 font-medium">Full Name *</label>
+                            <label className="block mb-2 text-base">Full Name *</label>
                             <input
                                 type="text"
                                 name="fullName"
                                 value={formData.fullName}
                                 onChange={handleChange}
-                                className={`w-full p-2 mb-4 border rounded ${errors.fullName ? 'border-red-500' : ''}`}
+                                className={`w-full p-2 mb-2 border rounded ${errors.fullName ? 'border-red-500' : ''}`}
                                 required
                             />
                             {errors.fullName && <p className="text-red-500">{errors.fullName}</p>}
-                            <label className="block mb-2 font-medium">Email</label>
+                            <label className="block mb-2 text-base">Email</label>
                             <input
                                 type="email"
                                 name="email"
                                 value={formData.email}
                                 onChange={handleChange}
-                                className="w-full p-2 mb-4 border rounded"
+                                className="w-full p-2 mb-2 border rounded"
                             />
-                            <label className="block mb-2 font-medium">Phone Number *</label>
+                            <label className="block mb-2 text-base">Phone Number *</label>
                             <div className="flex mb-4">
-                                <span className="inline-flex text-sm items-center px-3 border border-r-0 rounded-l bg-gray-200">AUG</span>
+                                <span className="inline-flex text-base items-center px-3 border border-r-0 rounded-l bg-gray-200">AUG</span>
                                 <input
                                     type="tel"
                                     name="phoneNumber"
@@ -140,118 +134,108 @@ const Checkout = () => {
                                 />
                             </div>
                             {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
-                            <label className="block mb-2 font-medium">Order Note (any message for us)</label>
+                            <label className="block mb-2 text-base">Order Note (any message for us)</label>
                             <textarea
                                 name="orderNote"
-                                value={formData.orderNote}
+                                defaultValue={formData.orderNote}
                                 onChange={handleChange}
-                                className="w-full p-2 mb-4 border rounded"
+                                className="w-full max-h-[100px] p-2 mb-2 border rounded"
                             ></textarea>
+
                         </div>
                         <div className="mb-6">
                             <h2 className="text-xl font-semibold mb-4">2. Delivery Address</h2>
-                            <label className="block mb-2 font-medium">City / District *</label>
+                            <label className="block mb-2 text-base">City / District *</label>
                             <input
                                 name="city"
                                 value={formData.city}
                                 onChange={handleChange}
-                                className={`w-full p-2 mb-4 border rounded ${errors.city ? 'border-red-500' : ''}`}
+                                className={`w-full p-2 mb-2 border rounded ${errors.city ? 'border-red-500' : ''}`}
                                 required
                             />
                             {errors.city && <p className="text-red-500">{errors.city}</p>}
-                            <label className="block mb-2 font-medium">Address *</label>
+                            <label className="block mb-2 text-base">Address *</label>
                             <input
                                 type="text"
                                 name="address"
                                 value={formData.address}
                                 onChange={handleChange}
-                                className={`w-full p-2 mb-4 border rounded ${errors.address ? 'border-red-500' : ''}`}
+                                className={`w-full p-2 mb-2 border rounded ${errors.address ? 'border-red-500' : ''}`}
                                 required
                             />
                             {errors.address && <p className="text-red-500">{errors.address}</p>}
-                            <label className="block mb-2 font-medium">PIN code</label>
+                            <label className="block mb-2 text-base">PIN code</label>
                             <input
                                 type="text"
                                 name="landmark"
                                 value={formData.landmark}
                                 onChange={handleChange}
-                                className="w-full p-2 mb-4 border rounded"
+                                className="w-full p-2 mb-2 border rounded"
                             />
                         </div>
                         <div className="mb-6">
                             <h2 className="text-xl font-semibold mb-4">3. Payment Methods</h2>
-                            <div className="mb-4">
-                                <input
-                                    type="radio"
-                                    name="paymentMethod"
-                                    value="Cash on delivery"
-                                    checked={paymentMethod === 'Cash on delivery'}
-                                    onChange={handlePaymentMethodChange}
-                                    className="mr-2"
-                                />
-                                <label className="font-medium">Cash on delivery</label>
-                            </div>
-                            {paymentMethod === 'Cash on delivery' && (
-                                <div className="mb-4">
-                                    <button
-                                        type="submit"
-                                        className="w-full p-3 bg-blue-500 text-white font-bold rounded"
-                                    >
-                                        COD
-                                    </button>
-                                </div>
-                            )}
-                            <div className="mb-4">
-                                <input
-                                    type="radio"
-                                    name="paymentMethod"
-                                    value="Card Payment"
-                                    checked={paymentMethod === 'Card Payment'}
-                                    onChange={handlePaymentMethodChange}
-                                    className="mr-2"
-                                />
-                                <label className="font-medium">Card Payment</label>
-                            </div>
                             {paymentMethod === 'Card Payment' && (
                                 <div className="mb-4">
-                                    <label className="block mb-2 font-medium">Card Number</label>
-                                    <input
-                                        type="text"
-                                        name="cardNumber"
-                                        value={cardDetails.cardNumber}
-                                        onChange={handleCardChange}
-                                        className={`w-full p-2 mb-4 border rounded ${errors.cardNumber ? 'border-red-500' : ''}`}
-                                    />
-                                    {errors.cardNumber && <p className="text-red-500">{errors.cardNumber}</p>}
-                                    <label className="block mb-2 font-medium">Expiry Date</label>
-                                    <input
-                                        type="text"
-                                        name="expiryDate"
-                                        value={cardDetails.expiryDate}
-                                        onChange={handleCardChange}
-                                        className={`w-full p-2 mb-4 border rounded ${errors.expiryDate ? 'border-red-500' : ''}`}
-                                    />
-                                    {errors.expiryDate && <p className="text-red-500">{errors.expiryDate}</p>}
-                                    <label className="block mb-2 font-medium">CVV</label>
-                                    <input
-                                        type="text"
-                                        name="cvv"
-                                        value={cardDetails.cvv}
-                                        onChange={handleCardChange}
-                                        className={`w-full p-2 mb-4 border rounded ${errors.cvv ? 'border-red-500' : ''}`}
-                                    />
-                                    {errors.cvv && <p className="text-red-500">{errors.cvv}</p>}
+                                    <div className='mb-4'>
+                                        <label className="block mb-1 text-sm">Card Number</label>
+                                        <div className='flex items-center gap-4'>
+                                            <input
+                                                type="text"
+                                                name="cardNumber"
+                                                value={cardDetails.cardNumber}
+                                                onChange={handleCardChange}
+                                                className={`w-full p-2 border rounded ${errors.cardNumber ? 'border-red-500' : ''}`}
+                                            />
+                                            <div className='flex gap-1 items-center'>
+                                                <img className="w-20" src={visa} alt="Visa" />
+                                                <img className="w-20" src={master} alt="MasterCard" />
+                                                <img className="w-20" src={am} alt="American Express" />
+                                            </div>
+                                        </div>
+                                        {errors.cardNumber && <p className="text-red-500 mt-1">{errors.cardNumber}</p>}
+                                    </div>
+
+                                    <div className='flex w-full gap-4'>
+                                        <div className='w-full'>
+
+                                            <label className="block mb-1 text-sm">Expiry Date</label>
+
+                                            <input
+                                                type="text"
+                                                name="expiryDate"
+                                                value={cardDetails.expiryDate}
+                                                onChange={handleCardChange}
+                                                className={`w-full p-2 mb-2 border rounded ${errors.expiryDate ? 'border-red-500' : ''}`}
+                                            />
+                                            {errors.expiryDate && <p className="text-red-500">{errors.expiryDate}</p>}
+                                        </div>
+                                        <div className='w-full'>
+
+                                            <label className="block mb-1 text-sm">CVV</label>
+                                            <input
+                                                type="text"
+                                                name="cvv"
+                                                value={cardDetails.cvv}
+                                                onChange={handleCardChange}
+                                                className={`w-full p-2 mb-2 border rounded ${errors.cvv ? 'border-red-500' : ''}`}
+                                            />
+                                            {errors.cvv && <p className="text-red-500">{errors.cvv}</p>}
+                                        </div>
+                                    </div>
                                     <button
                                         type="submit"
-                                        className="w-full p-3 bg-blue-500 text-white font-bold rounded"
+                                        className="w-full p-3 mt-4 pay-btn text-white font-bold rounded"
                                     >
-                                        Pay
+                                        Pay Now
                                     </button>
                                 </div>
                             )}
                         </div>
                     </form>
                 </div>
+
+
                 <div className="w-full pt-6  order-summerry lg:sticky lg:top-4 self-start">
 
                     <div className="mb-6 bg-white rounded-lg shadow-md">
@@ -262,23 +246,23 @@ const Checkout = () => {
                                     <img className='w-20 h-20 object-cover rounded-lg' src={item.img} alt={item.title} />
                                     <div className="flex-1">
                                         <p className="font-semibold">{item.title}</p>
-                                        <p className="text-sm text-gray-600">Variant: {item.color} / {item.size}</p>
-                                        <p className="text-gray-800">रू {item.price} x {item.quantity}</p>
+                                        <p className="text-base text-gray-600">Variant: {item.color} / {item.size}</p>
+                                        <p className="text-gray-800">$ {item.price} x {item.quantity}</p>
                                     </div>
                                 </div>
                             ))}
                         </div>
                         <div className="flex justify-between px-6 py-4">
                             <span className="font-semibold">Sub-total</span>
-                            <span>रू {calculateTotal()}</span>
+                            <span>$ {calculateTotal()}</span>
                         </div>
                         <div className="flex justify-between px-6 py-4">
                             <span className="font-semibold">Delivery Charge</span>
-                            <span>रू 100</span>
+                            <span>$ 100</span>
                         </div>
                         <div className="flex justify-between px-6 py-4">
                             <span className="font-semibold text-lg">Total</span>
-                            <span className="text-lg font-semibold">रू {calculateTotal() + 100}</span>
+                            <span className="text-lg font-semibold">$ {calculateTotal() + 100}</span>
                         </div>
                     </div>
                 </div>
@@ -288,3 +272,9 @@ const Checkout = () => {
 };
 
 export default Checkout;
+
+
+
+
+
+
